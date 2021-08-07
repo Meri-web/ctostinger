dbd9fa570596ca411f4a6572c13d3243db37b2ad
Meri <merchkolyan@gmail.com>
2021-08-07 16:30:52 +0400

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,13 +1,4 @@
 $(document).ready(function(){
-    $(".home-carousel").owlCarousel({
-        items:1,
-        animateOut: 'fadeOut',
-        autoplay:true,
-        autoplayTimeout:5000,
-        autoplayHoverPause:true,
-        loop: true,
-        dots: true
-    });
     $('.mobile-menu-bar').on('click', function () {
         $('.mobile-menu-content').addClass('open')
     });
